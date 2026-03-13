5043d78a182f3ddaf7edd91c8ecf5719311a0923
Kamran Wani <kamraanwani@gmail.com>
2026-03-13 11:19:40 +0530

    fix

--- server.js
+++ server.js
@@ -15,8 +15,15 @@ dotenv.config();
 const app = express();
 
 // ===== CORS (needed for sessions) =====
+const cors = require("cors");
+
 app.use(cors({
-  origin: "https://rfp-track.netlify.app"
+  origin: [
+    "https://rfp-track.netlify.app",
+    "http://127.0.0.1:5500",
+    "http://localhost:5500"
+  ],
+  credentials: true
 }));
 
 
